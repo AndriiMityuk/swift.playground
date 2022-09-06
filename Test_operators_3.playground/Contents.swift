05a654fd82141f09a173c059e06934ddec4cd479
Andrii Mytiuk <mityuk83@gmail.com>
2022-09-06 10:59:30 +0300

    #3 cheese

---
+++ Test_operators_3.playground/Contents.swift
@@ -0,0 +1,13 @@
+
+
+let coordinateFigure = (x: 2,y: 8)
+
+let statusXY = (coordinateFigure.x + coordinateFigure.y) % 2
+
+if statusXY == 0 {
+    print ("The cell is black!")
+} else {
+    print ("The cell is wight!")
+}
+
+
