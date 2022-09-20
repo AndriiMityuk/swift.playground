992c3a509f095f3b7be208a451f7715a2849284a
Andrii Mytiuk <mityuk83@gmail.com>
2022-09-20 22:08:13 +0300

    chess board builder

--- testDictionary3.playground/Contents.swift
+++ testDictionary3.playground/Contents.swift
@@ -11,10 +11,40 @@ if statusXY == 0 {
 }
 */
 
-var letter  = "abcdefgh"
-var number = "12345678"
-var nameCell = [Int]()
+let letter  = ["a","b","c","d","e","f","g","h"]
+let number = [8,7,6,5,4,3,2,1]
+var leterIndex : Int
+var chessTable = [String: Bool]()
 
+
+for a in letter {
+    leterIndex = letter.firstIndex(of: a)! + 1
+    for n in number {
+        let cell = a + String(n)
+        if (leterIndex + n) % 2 == 0 {
+            chessTable[cell] = false // black
+            //print(cell + " - black")
+            continue
+        }
+        chessTable[cell] = true // white
+        //print(cell + " - white")
+   }
+}
+
+print(chessTable["a5"]!)
+
+
+
+
+
+
+
+
+
+
+
+/*
+var nameCell = [Int]()
 // створено масив відповідності букв порядковому номеру
 var x = 1
 for _ in letter {
@@ -28,6 +58,7 @@ for _ in letter {
 var cheesTable = [String: Bool]()
 var cheesTable1 = [String]()
 
+/*
 for i in letter {
     for j in number {
         cheesTable1.append(String(i) + String(j))
@@ -37,4 +68,6 @@ for i in letter {
     }
 }
 print(cheesTable1)
-cheesTable1.
+*/
+
+
