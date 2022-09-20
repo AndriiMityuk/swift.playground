18a5c9ce4128fb9fbe2a2a928e4658ffee6284be
Andrii Mytiuk <mityuk83@gmail.com>
2022-09-20 10:52:36 +0300

    dictionary1

---
+++ testDictionary1.playground/Contents.swift
@@ -0,0 +1,19 @@
+// dictionary1
+
+
+var magazine = ["Tom Riddle": 5, "Mike Madisson": 3, "Jack Hook": 4, "Jon Silver": 5, "Bred Brown": 3]
+
+magazine.updateValue(4, forKey: "Tom Riddle")
+
+magazine["Nick Jonson"] = 5
+magazine["Jim Beam"] = 4
+
+magazine["Jack Hook"] = nil
+magazine["Jon Silver"] = nil
+
+var summaValue = 0
+for (_, studentValue) in magazine {
+    summaValue += studentValue
+}
+summaValue
+var averageValue : Double = Double(summaValue) / Double(magazine.count)
