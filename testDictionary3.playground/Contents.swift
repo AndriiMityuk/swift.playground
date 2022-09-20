5bcaf946ee91f433c360e746fa58275e1b484676
Andrii Mytiuk <mityuk83@gmail.com>
2022-09-20 23:16:38 +0300

    chess table finish

--- testDictionary3.playground/Contents.swift
+++ testDictionary3.playground/Contents.swift
@@ -1,15 +1,4 @@
 //dictionary 3  Chess table
-/*
-let coordinateFigure = (x: 5,y: 4)
-
-let statusXY = (coordinateFigure.x + coordinateFigure.y) % 2
-
-if statusXY == 0 {
-    print ("The cell is black!")
-} else {
-    print ("The cell is wight!")
-}
-*/
 
 let letter  = ["a","b","c","d","e","f","g","h"]
 let number = [8,7,6,5,4,3,2,1]
@@ -30,44 +19,4 @@ for a in letter {
         //print(cell + " - white")
    }
 }
-
-print(chessTable["a5"]!)
-
-
-
-
-
-
-
-
-
-
-
-/*
-var nameCell = [Int]()
-// створено масив відповідності букв порядковому номеру
-var x = 1
-for _ in letter {
-    nameCell.append(x)
-    x += 1
-}
-
-//var q = String(nameCell[0].description)
-
-
-var cheesTable = [String: Bool]()
-var cheesTable1 = [String]()
-
-/*
-for i in letter {
-    for j in number {
-        cheesTable1.append(String(i) + String(j))
-        //if ((i + Int(j)!) % 2) == 0 {
-            
-      //  }
-    }
-}
-print(cheesTable1)
-*/
-
-
+print(chessTable)
